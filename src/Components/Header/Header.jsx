f1b62f78e45eb37abad4702ef841b3f0014a063d
eaw1ch <eaw1ch@mail.ru>
2024-03-14 19:58:51 +0300

    mobile-menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,49 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-scroll'
 import './Header.css'
+
 import logo from '../../assets/images/logo.svg'
+import { ReactComponent as Burger } from '../../assets/icons/hamburger-menu.svg'
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <nav className="navigation">
-            <header className="header">
-                <img className="logo" src={logo} alt="" />
+        <header className="header">
+            <img className="logo" src={logo} alt="" />
+            <nav className={`navigation ${isOpen ? 'active' : ''}`}>
                 <ul className="header__block">
-                    <li>
+                    <li className="header__block-element">
                         <Link
-                            className="header__block-element underline"
+                            className="underline"
                             to="about"
                             smooth={true}
                             duration={1000}
+                            onClick={() => setIsOpen(!isOpen)}
                         >
                             О нас
                         </Link>
                     </li>
 
-                    <li>
+                    <li className="header__block-element">
                         <Link
-                            className="header__block-element underline"
+                            className="underline"
                             to="coffee-menu"
                             smooth={true}
                             duration={1000}
+                            onClick={() => setIsOpen(!isOpen)}
                         >
                             Напитки
                         </Link>
                     </li>
 
-                    <li>
+                    <li className="header__block-element">
                         <Link
-                            className="header__block-element underline"
+                            className="underline"
                             to="contacts"
                             smooth={true}
                             duration={1000}
+                            onClick={() => setIsOpen(!isOpen)}
                         >
                             Контакты
                         </Link>
                     </li>
                 </ul>
-            </header>
-        </nav>
+            </nav>
+            <button
+                className="header__menu-button"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <Burger className="menu-icon" />
+            </button>
+        </header>
     )
 }
 
